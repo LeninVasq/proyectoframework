1709bacd4a3b671d40c68194a6353db91e9e17d2
LeninVasq <leninvasques12345@gmail.com>
2024-11-18 20:27:37 -0600

    finals

--- src/main/java/com/proyecto/proyectoso/entity/Asistencia.java
+++ src/main/java/com/proyecto/proyectoso/entity/Asistencia.java
@@ -1,4 +1,18 @@
 package com.proyecto.proyectoso.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Asistencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_asistencia;
+    @Column(name= "estado", unique = true,nullable = false)
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "incripcion_id", referencedColumnName = "incripcion_id")
+    private inscripciones incripcion_id;
+
 }
